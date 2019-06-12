21462d870d70c38f71ce10d4db10f016c7db2853
Java Developer <Java Developer@192.168.0.9>
2019-06-12 21:21:09 +0530

    Data Jpa First application

---
+++ src/main/java/com/app/ProfilesEx1Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesEx1Application.class, args);
+	}
+
+}
